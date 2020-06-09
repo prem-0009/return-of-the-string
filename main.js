3711629c9715ce1f2d099786dce8ee5c88d2c2d8
prem <prem.sherpa@codeimmersives.com>
2020-06-09 08:13:30 -0400

    one left

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 /*******************
  * YOUR CODE HERE! *
  *******************/
+function yell(str){
+  return str+'!';
+ console.log(str) 
+}
+
+function getFirstCharacter(str){
+  return str[0];
+}
+
+function getLastCharacter(str){
+  return str[str.length-1];
+}
+
+function getOneCharacter(str, num){
+  return str[num];
+}
+
+function getTwoCharacters(str, num1, num2){
+  return str[num1]+str[num2];
+}
+
+function makeCapitalized(str){
+  return str.toUpperCase();
+}
+
+function yellLouder(str){
+  return str.toUpperCase()+'!!!';
+}
+
+function getInitials(str){
+  const splitstr=str.split(" ");
+  const first=splitstr[0][0].toUpperCase();
+  
+  const last=splitstr.slice(-2)[0][0].toUpperCase();
+  
+  return first+'.'+last+'.'
+  
+}
+
+ 
 
 
-
-
-
+getInitials('prem','sherpa')
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
